bd2b1f63eeca5628a752a1c55ca67c77e6127ff4
BragmaDev <mh.bragge@gmail.com>
2025-02-11 18:20:59 +0200

    Added ex5
    Co-authored-by: Lionbull <Lionbull@users.noreply.github.com>
    Co-authored-by: Hageli <Hageli@users.noreply.github.com>
    Co-authored-by: spoilaro <spoilaro@users.noreply.github.com>
    Co-authored-by: lvzn <lvzn@users.noreply.github.com>

--- ex5/main.c
+++ ex5/main.c
@@ -1,55 +1,54 @@
 #define F_CPU 16000000UL // UL stands for Unsigned Long
 
-#include "lcd.h"
 #include <avr/interrupt.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <util/delay.h>
 #include <avr/io.h>
-#include <stdbool.h>
 
-volatile bool adc_conv_complete = false;
-volatile uint16_t voltage_to_digital_value = 0;
 
 void setup() {
-    DDRC &= ~(1 << PC3);
-    TCCR0A = 0x02; //See datasheet p. 104
-    TCCR0B = 0x05; //See datasheet p. 107
-    OCR0A = 0xFF; //See datasheet p. 108
-    TIMSK0 |= (1 << 1); //See datasheet p. 109
-    TCNT0 = 0; //See datasheet p. 108
+    DDRB |= (1 << PB5);
+    TCNT1 = 0;  // Reset the counter
+    TCCR1B = 0; // Reset TCCRB1
+    TCCR1A |= (1 << 6); //See datasheet p. 154 and 155
+    TCCR1A |= (1 << 0); //See datasheet p. 154
+    TCCR1B |= (1 << 4); //See datasheet p. 156
+    TIMSK1 |= (1 << 1); // Enable timer/counter1 interrupt in TIMSK1 (See datasheet p. 161)
+    OCR1A = 30534; // C4 262 Hz, no prescaler
     sei();
-    ADMUX |= 0b01000000; //See datasheet p. 248. Sets AV_cc as voltage reference.
-    ADMUX |= 0b00000011; //See datasheet p. 249. Set A3 as input channel.
-    ADCSRA |= (1 << 7); // Enable ADC, see datasheet p. 249
-    ADCSRA |= (1 << 5); // Enable ADC auto trigger, see datasheet p. 249
-    ADCSRA |= (1 << 3); // Enable ADC interrupt, see datasheet p. 250
-    ADCSRA |= 0b00000101; // Set prescaler to 32, see datasheet p. 250
-    ADCSRB |= 0b00000011; // Set ADC to trigger with timer/counter0 compare match A, see datasheet p. 251
-    sei();
-    lcd_init(LCD_DISP_ON);
 }
 
 int main(void)
 {
     setup();
 
-    while (1) { 
-        if (adc_conv_complete) {
-            char buffer[10];
-            itoa(voltage_to_digital_value, buffer, 10);
-            lcd_clrscr();
-            lcd_puts(buffer);
-            adc_conv_complete = false;
-        }   
+    while (1) {
+        // C4 262 Hz, no prescaler
+        OCR1A = 30534;
+        TCCR1B |= (1 << 0); // for no prescaler
+        _delay_ms(1000);
+
+        // E3 165 Hz, no prescaler
+        OCR1A = 48485;
+        _delay_ms(1000);
+        
+        // F2 87 Hz, 8 prescaler
+        OCR1A = 11494; 
+        TCCR1B &= ~(1 << 0); // reset prescaler
+        TCCR1B |= (1 << 1); // for 8 prescaler
+        _delay_ms(1000);
+        
+        // D0 18 Hz, 64 prescaler
+        OCR1A = 6944;
+        TCCR1B |= (1 << 0); // for 64 prescaler
+        _delay_ms(1000);
+        TCCR1B &= ~(1 << 0); // reset prescaler 
+        TCCR1B &= ~(1 << 1); // reset prescaler    
     }
+
+    return 0;
 }
 
-ISR (TIMER0_COMPA_vect) {
+ISR (TIMER1_COMPA_vect) {
 
 }
 
-ISR (ADC_vect) {
-  voltage_to_digital_value = ADC;
-  adc_conv_complete = true;
-}
